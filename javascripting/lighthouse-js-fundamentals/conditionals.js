3fc24b296a4f46dcfdbf97ae6ba8e5d39d220304
mauroapjr <mauroapjr@outlook.com>
2022-12-28 12:22:50 -0500

    add conditional examples

--- javascripting/lighthouse-js-fundamentals/conditionals.js
+++ javascripting/lighthouse-js-fundamentals/conditionals.js
@@ -1,3 +1,4 @@
+/*
 const raining = true;
 const cold = false;
 
@@ -47,3 +48,85 @@ if (!raining) {
 }
 
 //-------------------------------//
+
+let age = 12;
+
+if ( age < 13) {
+  console.log("You should go to Elementary School");
+} else if (age > 13 && < 18) {
+  console.log("You should go to Secondary School");
+} else {
+  console.log("Your choice is Lighthouse Labs");
+}
+
+//-------------------------------//
+
+var eatsPlants = false;
+var eatsAnimals = true;
+
+/*
+ * Test your code agaist the followig possible input/output combinations of (`eatsPlants`, `eatsAnimals`, expected output):
+ * - (true, true, omnivore)
+ * - (false, true, carnivore)
+ * - (true, false, herbivore)
+ * - (false, false, undefined)
+
+var category = eatsPlants ? (eatsAnimals ? "omnivore" : "herbivore") : (eatsAnimals ? "carnivore" : "undefined");
+
+
+console.log(category);
+
+//-------------------------------//
+
+var tier = "nsfw deck";
+var output = "You’ll receive "
+
+switch (tier) {
+  case "deck of legends":
+    output += "a custom card, ";
+  case "collector's deck":
+    output += "a signed version of the Exploding Kittens deck, ";
+  case "nsfw deck":
+    output += "one copy of the NSFW (Not Safe for Work) Exploding Kittens card game and ";
+  default:
+    output += "one copy of the Exploding Kittens card game.";
+}
+
+console.log(output);
+
+//-------------------------------//
+
+var education = 'a high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+switch (education) {
+    case "no high school diploma":
+        salary = 25636;
+        break;
+    case "a high school diploma":
+        salary = 35256;
+        break;
+    case "an Associate's degree":
+        salary = 41496;
+        break;
+    case "a Bachelor's degree":
+        salary = 59124;
+        break;
+    case "a Master's degree":
+        salary = 69732;
+        break;
+    case "a Professional degree":
+        salary = 89960;
+        break;
+    case "a Doctoral degree":
+        salary = 84396;
+        break;
+}
+
+
+console.log("In 2015, a person with "+education+" earned an average of $"+salary.toLocaleString("en-US")+"/year.");
+
+//-------------------------------//
+*/
