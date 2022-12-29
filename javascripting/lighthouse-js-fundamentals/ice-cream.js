6338326b3bc3e696232a7de1864c5fdc9298d6eb
mauroapjr <mauroapjr@outlook.com>
2022-12-28 19:27:37 -0500

    almost there solving this work

---
+++ javascripting/lighthouse-js-fundamentals/ice-cream.js
@@ -0,0 +1,20 @@
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry",
+];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+let firstFlavour = iceCreamFlavours[0];
+console.log(firstFlavour);
+
+let lastFlavour = iceCreamFlavours[iceCreamFlavours.length - 1];
+console.log(lastFlavour);
+
+const size = iceCreamFlavours.lenght;
+console.log(size);
