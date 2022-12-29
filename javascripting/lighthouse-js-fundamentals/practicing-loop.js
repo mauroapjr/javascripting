08e8c9730ff3b06dce721e93ee8cb0d3ed76553e
mauroapjr <mauroapjr@outlook.com>
2022-12-29 16:09:00 -0500

    practice update

--- javascripting/lighthouse-js-fundamentals/practicing-loop.js
+++ javascripting/lighthouse-js-fundamentals/practicing-loop.js
@@ -1,4 +1,4 @@
-let loopy = 0;
+/*let loopy = 0;
 
 for (let loopy = 100; loopy <= 200; loopy++) {
   if (loopy % 3 == 0) console.log(loopy);
@@ -9,5 +9,23 @@ function replaceNumbers(string, replacement) {
 }
 console.log(replaceNumbers(loopy, "Loopy"));
 
-//let replaced = loopy.replace(/ \d /+g, "Loopy");
-//console.log(loopy.replace(/[loopy % 3 == 0]/g, "Loopy"));
+let replaced = loopy.replace(/ \d / + g, "Loopy");
+console.log(loopy.replace(/[loopy % 3 == 0]/g, "Loopy"));
+*/
+
+for (let x = 100; x <= 200; x++) {
+  switch (x <= 200) {
+    case x % 3 === 0 && x % 4 !== 0:
+      console.log("Loopy");
+      break;
+    case x % 4 === 0 && x % 3 !== 0:
+      console.log("Lighthouse");
+      break;
+    case x % 3 === 0 && x % 4 === 0:
+      console.log("LoopyLighthouse");
+      break;
+    default:
+      console.log(x);
+      break;
+  }
+}
