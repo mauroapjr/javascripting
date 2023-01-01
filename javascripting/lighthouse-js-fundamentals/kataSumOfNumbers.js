2af5280b5458d8d90037d25105cdf3e5c39f3e64
mauroapjr <mauroapjr@outlook.com>
2023-01-01 16:08:34 -0500

    Kata 1 challenge

--- javascripting/lighthouse-js-fundamentals/kataSumOfNumbers.js
+++ javascripting/lighthouse-js-fundamentals/kataSumOfNumbers.js
@@ -1,1 +1,20 @@
-const sumLargestNumbers = function (data) {};
+const sumLargestNumbers = function (data) {
+  let firstBig = 0;
+  let secondBig = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] > firstBig) {
+      firstBig = data[i];
+    }
+  }
+
+  for (let j = 0; j < data.length; j++) {
+    if (data[j] > secondBig && data[j] < firstBig) {
+      secondBig = data[j];
+    }
+  }
+  return firstBig + secondBig;
+};
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
