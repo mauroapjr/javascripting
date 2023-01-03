94ea7f8f9d4db4ed52feb907d1cc807811f224e5
mauroapjr <mauroapjr@outlook.com>
2023-01-03 17:00:04 -0500

    loopy lighthouse stretch

--- javascripting/lighthouse-js-fundamentals/loopyLighthouse2.js
+++ javascripting/lighthouse-js-fundamentals/loopyLighthouse2.js
@@ -1,5 +1,16 @@
-function loopyLighthouse(range, multiples, words) {
-  for (let i = 15; i < 90; i++) if (range[i] / 2 === "Batty") return range;
-}
+const loopyLighthouse = (range, multiples, words) => {
+  let result = "";
+  for (result = range[0]; result <= range[1]; result++) {
+    if (result % multiples[0] === 0 && result % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (result % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (result % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(result);
+    }
+  }
+};
 
 console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
